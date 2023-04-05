e9157d3891569f64598df22dd452ab0e486e3607
mahmud0v <islomibndilmurod@gmail.com>
2023-04-05 14:48:22 -0700

    initial commit

--- app/src/main/java/uz/tuit/tuitlens/MainActivity.kt
+++ app/src/main/java/uz/tuit/tuitlens/MainActivity.kt
@@ -1,11 +1,30 @@
 package uz.tuit.tuitlens
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+        countDownTimer()
+
     }
+
+
+    private fun countDownTimer(){
+        val timer = object : CountDownTimer(2000L,1000L){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                startActivity(Intent(this@MainActivity,SecondActivity::class.java))
+            }
+
+        }.start()
+    }
+
 }
---
+++ app/src/main/java/uz/tuit/tuitlens/SecondActivity.kt
@@ -0,0 +1,15 @@
+package uz.tuit.tuitlens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_pager_home)
+
+    }
+
+
+}
